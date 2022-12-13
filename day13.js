d281528f8d4c2ea464b37f095caf854d8787d617
Fancy <maciekgplay@gmail.com>
2022-12-13 14:19:08 +0100

    Add day 13 solution

---
+++ day13.js
@@ -0,0 +1,35 @@
+/*  Emojify!
+
+Popular services like Slack and Github allow for emoji shortscodes, meaning 
+they will detect when a word in a sentence begins and ends with a colon (:)
+and automatically replace that word with an emoji. 
+
+These shortcodes allow users to add an emoji to their messages by typing a 
+code rather than searching for an emoji from a list. 
+
+For example, typing :smile: will replace that text with 😊 
+
+*/
+
+const emojis = {
+    "smile": "😊",
+    "angry": "😠",
+    "party": "🎉",
+    "heart": "💜",
+    "cat":   "🐱",
+    "dog":   "🐕"
+}
+
+const emojifyWord = (word) => {
+    const formattedWord = word.startsWith(':') && word.endsWith(':') ? word.slice(1,-1) : word
+    return emojis[formattedWord] ? emojis[formattedWord] : formattedWord
+}
+
+const emojifyPhrase = (phrase) => phrase.split(' ').map((word) => emojifyWord(word)).join(' ')
+
+console.log(emojifyWord(":heart:"));
+console.log(emojifyWord(":flower:"));
+console.log(emojifyWord("elephant"));
+
+console.log(emojifyPhrase("I :heart: my :cat:"));
+console.log(emojifyPhrase("I :heart: my :elephant:"));
